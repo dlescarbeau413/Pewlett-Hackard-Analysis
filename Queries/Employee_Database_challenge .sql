85388fad3931f43d28c2764ac4f00d4a60c90fdf
dlescarbeau413 <dlescarbeau413@gmail.com>
2021-11-23 18:59:02 -0500

    Update

--- Queries/Employee_Database_challenge .sql
+++ Queries/Employee_Database_challenge .sql
@@ -7,12 +7,14 @@ e.last_name,
 t.title,
 t.from_date,
 t.to_date
+
 -- INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t 
 ON e.emp_no = t.emp_no
 WHERE birth_date between '1952-01-01' and '1955-12-31'
 ORDER BY e.emp_no;
+
 -------
 --Deliverable 1: 8-14
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -26,6 +28,7 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 --Deliverable 1: 15-21
+
 SELECT 
 COUNT(title),
 title
@@ -33,8 +36,10 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+
 ----------
--- Deliverable 2 1-11
+-- Deliverable 2: 1-11
 
 
 SELECT DISTINCT ON (e.emp_no)
