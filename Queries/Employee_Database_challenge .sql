2e7f972282d7ddc9b28ca92749899291c333056b
dlescarbeau413 <dlescarbeau413@gmail.com>
2021-11-23 18:32:53 -0500

    Update

--- Queries/Employee_Database_challenge .sql
+++ Queries/Employee_Database_challenge .sql
@@ -34,4 +34,23 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 ----------
+-- Deliverable 2 1-11
 
+
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM Employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t 
+ON de.emp_no = t.emp_no
+WHERE e.birth_date between ('1965-01-01') AND ('1965-12-31') 
+AND de.to_date = ('9999-01-01')
+;
